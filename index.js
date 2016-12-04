bf981fe7d55b24987d64e8862fb7f7bc0b457957
kevzettler <kevzettler@gmail.com>
2016-12-04 15:39:28 -0800

    concentrate on recursivlly returing arrays of siblings

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ function readChunkIndexRange(Buffer, chunkStartIndex, chunkEndIndex, accum){
     Object.assign(chunk, getChunkData(Buffer, chunk));
   }
 
+  //children
+  if(childContentByteLength > 0) {
+    return readChunkIndexRange(Buffer,
+                               chunk.definitionEndIndex+chunk.contentByteLength,
+                               chunk.definitionEndIndex+chunk.childContentByteLength,
+                               chunk);
+  }  
+
   //siblings
   if(chunk.totalContentLength < chunkEndIndex){
     return readChunkIndexRange(Buffer,
@@ -42,20 +50,13 @@ function readChunkIndexRange(Buffer, chunkStartIndex, chunkEndIndex, accum){
                                accum);
   }
 
-  //children
-  if(childContentByteLength > 0) {
-    return readChunkIndexRange(Buffer,
-                               chunk.definitionEndIndex+chunk.contentByteLength,
-                               chunk.definitionEndIndex+chunk.childContentByteLength,
-                               accum);
-  }
 
   return accum;
 }
 
 function MagicaVoxelParser(Buffer){
   var ret = {'VOX ': 150};
-  return readChunkIndexRange(Buffer, 8, Buffer.length, ret);
+  return readChunkIndexRange(Buffer, 20, Buffer.length, ret);
 }
 
 
@@ -63,3 +64,43 @@ fs.readFile("chr_fatkid.vox", function (err, Buffer) {
   if (err) throw err;
   console.log(JSON.stringify(MagicaVoxelParser(Buffer), null, 2));
 });
+
+
+/* {
+ *   "VOX ": 150,
+ *   "MAIN": {
+ *     "id": "MAIN",
+ *     "chunkStartIndex": 8,
+ *     "definitionEndIndex": 20,
+ *     "contentByteLength": 0,
+ *     "childContentByteLength": 2284,
+ *     "totalContentLength": 2304
+ *   },
+ *   "SIZE": {
+ *     "id": "SIZE",
+ *     "chunkStartIndex": 20,
+ *     "definitionEndIndex": 32,
+ *     "contentByteLength": 12,
+ *     "childContentByteLength": 0,
+ *     "totalContentLength": 44,
+ *     "stub": "data"
+ *   },
+ *   "XYZI": {
+ *     "id": "XYZI",
+ *     "chunkStartIndex": 44,
+ *     "definitionEndIndex": 56,
+ *     "contentByteLength": 1212,
+ *     "childContentByteLength": 0,
+ *     "totalContentLength": 1268,
+ *     "stub": "data"
+ *   },
+ *   "RGBA": {
+ *     "id": "RGBA",
+ *     "chunkStartIndex": 1268,
+ *     "definitionEndIndex": 1280,
+ *     "contentByteLength": 1024,
+ *     "childContentByteLength": 0,
+ *     "totalContentLength": 2304,
+ *     "stub": "data"
+ *   }
+ * }*/
