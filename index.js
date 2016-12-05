ed80db0913645603dbd1b3a8717e04aa5d11c960
kevzettler <kevzettler@gmail.com>
2016-12-04 17:09:41 -0800

    minimal return

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 const intByteLength = 4;
 
-function SIZEHandler(Buffer, chunk){
-  var readBitIndex = chunk.definitionEndIndex;
+function SIZEHandler(Buffer, contentStartBitIndex){
+  var readBitIndex = contentStartBitIndex;
   var sizex = Buffer.readInt32LE(readBitIndex);
   readBitIndex += 4;
 
@@ -18,8 +18,8 @@ function SIZEHandler(Buffer, chunk){
   };
 }
 
-function XYZIHandler(Buffer, chunk){
-  var readBitIndex = chunk.definitionEndIndex;
+function XYZIHandler(Buffer, contentStartBitIndex){
+  var readBitIndex = contentStartBitIndex;
   var numVoxels = Math.abs(Buffer.readInt32LE(readBitIndex));
   readBitIndex += 4;
 
@@ -36,8 +36,8 @@ function XYZIHandler(Buffer, chunk){
   return {voxelData: voxelData};
 }
 
-function RGBAHandler(Buffer, chunk){
-  var readBitIndex = chunk.definitionEndIndex;
+function RGBAHandler(Buffer, contentStartBitIndex){
+  var readBitIndex = contentStartBitIndex;
   
   var colors = new Array();
   for (var n = 0; n < 256; n++) {
@@ -57,8 +57,11 @@ const chunkHandlers = {
   RGBA: RGBAHandler
 };
 
-function getChunkData(Buffer, chunk){
-  return chunkHandlers[chunk.id] ? chunkHandlers[chunk.id](Buffer, chunk) : null;
+function getChunkData(Buffer, id, definitionEndIndex){
+  if(!chunkHandlers[id]){
+    throw "Unsupported chunk type " + id;
+  }
+  return chunkHandlers[id](Buffer, definitionEndIndex);
 }
 
 function readChunkIndexRange(Buffer, bufferStartIndex, bufferEndIndex, accum){
@@ -74,38 +77,37 @@ function readChunkIndexRange(Buffer, bufferStartIndex, bufferEndIndex, accum){
   var childContentByteLength = Buffer.readInt32LE(readBitIndex);
   readBitIndex += intByteLength;
 
-  var chunk = {
-    id: id,
-    bufferStartIndex: bufferStartIndex,
-    definitionEndIndex:  readBitIndex,
-    contentByteLength: chunkContentByteLength,
-    childContentByteLength: childContentByteLength,
-    totalEndIndex: readBitIndex + chunkContentByteLength + childContentByteLength
-  };
+  var bufferStartIndex = bufferStartIndex;
+  var definitionEndIndex =  readBitIndex;
+  var contentByteLength = chunkContentByteLength;
+  var childContentByteLength = childContentByteLength;
+  var totalEndIndex = readBitIndex + chunkContentByteLength + childContentByteLength;
+
+  var chunk = {};
 
   accum[id] = chunk;
   
-  if(chunk.contentByteLength == 0 && childContentByteLength == 0){
+  if(contentByteLength == 0 && childContentByteLength == 0){
     console.log("no content or children");
     return accum;
   }
 
-  if(chunk.contentByteLength && chunk.id){
-    Object.assign(chunk, getChunkData(Buffer, chunk));
+  if(contentByteLength && id){
+    Object.assign(chunk, getChunkData(Buffer, id, definitionEndIndex));
   }
 
   //read children
-  if(chunk.childContentByteLength > 0){
+  if(childContentByteLength > 0){
     return readChunkIndexRange(Buffer,
-                               chunk.definitionEndIndex+chunk.contentByteLength,
+                               definitionEndIndex+contentByteLength,
                                bufferEndIndex,
                                chunk);
   }
 
   //accumulate siblings
-  if(chunk.totalEndIndex != bufferEndIndex){
+  if(totalEndIndex != bufferEndIndex){
     return readChunkIndexRange(Buffer,
-                               chunk.totalEndIndex,
+                               totalEndIndex,
                                bufferEndIndex,
                                accum);
   }
