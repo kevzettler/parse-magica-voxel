ebff8e643df081ef78d3b4be98f8d38bcb6c6116
kevzettler <kevzettler@gmail.com>
2016-12-06 16:58:26 -0800

    buffer debugging

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Buffer = require('buffer').Buffer;
 const isBuffer = require('is-buffer');
 const intByteLength = 4;
 
+console.log("Buffer included??", Buffer);
+
 function SIZEHandler(Buffer, contentStartByteIndex){
   var readByteIndex = contentStartByteIndex;
   var sizex = Buffer.readInt32LE(readByteIndex);
@@ -130,7 +132,7 @@ function parseHeader(Buffer){
 
 function MagicaVoxelParser(BufferLikeData){
   var buffer = BufferLikeData;
-  
+  console.log('wtf is Buffer', Buffer);
   if(!isBuffer(buffer)){
     try {
       buffer = new Buffer( new Uint8Array(BufferLikeData) );
