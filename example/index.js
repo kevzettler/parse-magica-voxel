9e6bc3ec8d408df6317c14f6544d2e726cbe1a5b
Kev Zettler <kevzettler@gmail.com>
2022-02-10 17:36:59 -0800

    ghetto testing setup

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var parseMagicaVoxel = require('../src/index.js');
 
-fs.readFile("./chr_sword.vox", function (err, Buffer) {
+fs.readFile(__dirname+ "/chr_sword.vox", function (err, Buffer) {
   if (err) throw err;
   console.log(JSON.stringify(parseMagicaVoxel(Buffer), null, 2));
 });
